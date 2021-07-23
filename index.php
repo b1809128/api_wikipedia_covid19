e5ecbd93f7d719d8ae4fbff1acd8f7d33514e9a6
b1809128 <75192393+b1809128@users.noreply.github.com>
2021-07-23 21:03:50 +0700

    new php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php 
     require_once './simple_html_dom.php';
-    
+    $arrContextOptions = array(
+        "ssl"=>array(
+            "verify_peer"=>false,
+            "verify_peer_name"=>false
+        )
+    );
     // https://news.google.com/covid19/map?hl=vi&mid=%2Fm%2F01crd5&gl=VN&ceid=VN%3Avi
-    $content = file_get_html('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam');
+    $content = file_get_html('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam', false, stream_context_create($arrContextOptions));
     // echo $content;
     // $find = $content->find('th',20);
     // echo $find->plaintext;
