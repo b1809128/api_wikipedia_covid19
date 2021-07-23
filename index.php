4274ae62753543f975ef2350bd4168b918354580
b1809128 <75192393+b1809128@users.noreply.github.com>
2021-07-23 21:00:49 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php 
-    include './crawl.php';
+    require_once './simple_html_dom.php';
+    
+    // https://news.google.com/covid19/map?hl=vi&mid=%2Fm%2F01crd5&gl=VN&ceid=VN%3Avi
+    $content = file_get_html('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam');
+    // echo $content;
+    // $find = $content->find('th',20);
+    // echo $find->plaintext;
+
+    foreach ($content->find('th') as $key=>$element) {
+        echo $key ."--";
+        print_r($element->plaintext);
+        echo "<br>";
+    }
+
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
