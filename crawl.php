28e7fb1e19301f7d2716cb10748f1c7891aa46fd
b1809128 <nghoquochuy0902@gmail.com>
2021-07-25 14:33:29 +0700

    test

--- crawl.php
+++ crawl.php
@@ -7,25 +7,24 @@
         )
     );
     // https://news.google.com/covid19/map?hl=vi&mid=%2Fm%2F01crd5&gl=VN&ceid=VN%3Avi
+    // https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam
     $content = file_get_html('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam', false, stream_context_create($arrContextOptions));
     // echo $content;
-    // $find = $content->find('th',20);
-    // echo $find->plaintext;
+    // $find = $content->find('div',0);
+    // echo $find->outertext;
 
-    foreach ($content->find('th') as $key=>$element) {
-        echo $key ."--";
-        print_r($element->plaintext);
-        echo "<br>";
-    }
+    foreach($content->find('b') as $element)
+       echo $element->innertext . '<br>';
 
-
-
-    // for($i = 4; $i <66;$i++) {
-    //     $find = $content->find('.text-danger-new',$i)->plaintext;        
-    //     if($find != 0){
-    //         echo substr($find,1)."<br>";
-    //     }
-    //     else{
-    //         echo $find."<br>";
-    //     }
+    // foreach ($content->find('._1oiuo') as $key=>$element) {
+    //     echo $key ."--";
+    //     print_r($element->plaintext);
+    //     echo "<br>";
     // }
+
+    
+?>
+
+
+    
+    
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php 
-    include './crawl.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,11 +5,98 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Covid-19 Report</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
+    <div class="app">
+        <div class="container">
+            <button class="btn">Cần Thơ</button>
+            <div class="card">
+                <?php
+                require_once './simple_html_dom.php';
+                $arrContextOptions = array(
+                    "ssl" => array(
+                        "verify_peer" => false,
+                        "verify_peer_name" => false
+                    )
+                );
 
+                $content = file_get_html('https://covid.cantho.gov.vn/?fbclid=IwAR006ORto0SYOL-Iz5VqgRDZpzAbyPCqM4Dz6TnhLvRCL-XcHKkRlAQtMa0', false, stream_context_create($arrContextOptions));
+
+
+
+                ?>
+
+                <div class="card-item infected cantho">
+                    <div class="card-title infected-title">F0</div>
+                    <div class="card-content">
+                        <div class="card-text">Tổng F0</div>
+                        <div class="card-number"><?= $content->find('span', 8)->plaintext ?></div>
+                    </div>
+                </div>
+                <div class="card-item recovered cantho">
+                    <div class="card-title recovered-title">F1</div>
+                    <div class="card-content">
+                        <div class="card-text">Tổng F1</div>
+                        <div class="card-number"><?= $content->find('span', 11)->plaintext ?></div>
+                    </div>
+                </div>
+                <div class="card-item treatment cantho">
+                    <div class="card-title treatment-title">F2</div>
+                    <div class="card-content">
+                        <div class="card-text">Tổng F2</div>
+                        <div class="card-number"><?= $content->find('span', 14)->plaintext ?></div>
+                    </div>
+                </div>
+                <div class="card-item death cantho">
+                    <div class="card-title death-title">i</div>
+                    <div class="card-content">
+                        <div class="card-text">Tử vong</div>
+                        <div class="card-number"><?= $content->find('span', 17)->plaintext ?></div>
+                    </div>
+                </div>
+            </div>
+            <button class="btn">Việt Nam</button>
+            <div class="card">
+                <?php
+                require_once './simple_html_dom.php';
+                $arrContextOptions = array(
+                    "ssl" => array(
+                        "verify_peer" => false,
+                        "verify_peer_name" => false
+                    )
+                );
+
+                $vietnam = file_get_html('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam', false, stream_context_create($arrContextOptions));
+
+                ?>
+                <div class="card-item infected vietnam">
+                    <div class="card-text background_1">Số ca nhiễm</div>
+                    <div class="card-number"><?= $vietnam->find('th', 20)->plaintext ?></div>
+                </div>
+                <div class="card-item recovered vietnam">
+                    <div class="card-text background_2">Khỏi</div>
+                    <div class="card-number"><?= $vietnam->find('th', 23)->plaintext ?></div>
+                </div>
+                <div class="card-item treatment vietnam">
+                    <div class="card-text background_3">Đang điều trị</div>
+                    <div class="card-number"><?= $vietnam->find('th', 21)->plaintext ?></div>
+                </div>
+                <div class="card-item death vietnam">
+                    <div class="card-text background_4">Tử vong</div>
+                    <div class="card-number"><?= $vietnam->find('th', 24)->plaintext ?></div>
+                </div>
+            </div>
+
+            <div class="image-map">
+                <h4><?= $vietnam->find('b', 0)->innertext ?></h4>
+                <?= $vietnam->find('a', 3)->innertext ?>
+            </div>
+
+        </div>
+    </div>
 </body>
 
 </html>
