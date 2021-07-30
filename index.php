24937b3a81b87930ad8eee2f16f6cb6c254eb40f
b1809128 <nghoquochuy0902@gmail.com>
2021-07-30 13:57:14 +0700

    update icon website

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Covid-19 Report</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.css" >
+    <link rel="icon" href="https://react-qh.herokuapp.com/favicon.ico">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 </head>
 
