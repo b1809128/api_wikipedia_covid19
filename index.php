6545059de77381277cf819f62377fcdbac1e4eaf
b1809128 <nghoquochuy0902@gmail.com>
2021-07-31 10:17:04 +0700

    add new data vietnam

--- index.php
+++ index.php
@@ -63,11 +63,13 @@
                 );
 
                 $vietnam = file_get_html('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam', false, stream_context_create($arrContextOptions));
+    $rss = file_get_html('https://rsstin.com/topic/covid-19?utm_source=coccoc_context&utm_medium=CPC&utm_campaign=RSSTIN%2ECOM&utm_term=covid&utm_content=36764798&md=_0u7e50vdaa*Cr3GhsODtX4tKHPqZpuei1-JLidqizJd6qqz68t50KG57tcAl84vgeC2uxhJSiP8E.', false, stream_context_create($arrContextOptions));
 
                 ?>
                 <div class="card-item infected vietnam">
                     <div class="card-text background_1">Số ca nhiễm</div>
                     <div class="card-number"><?= $vietnam->find('th', 20)->plaintext ?></div>
+                    <div>Hôm nay: +<?= $rss->find('strong',1)->innertext ?></div>
                 </div>
                 <div class="card-item recovered vietnam">
                     <div class="card-text background_2">Khỏi</div>
